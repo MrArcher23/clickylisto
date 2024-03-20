51b094aabbbfcb8b14266626a91e2e9d9f331a4d
Raul Gallegos <rgh2309@gmail.com>
2024-03-20 14:40:34 -0600

    agregando detalles y preparando para primer despliegue

---
+++ src/components/ActionButton/ActionButton.tsx
@@ -0,0 +1,28 @@
+import { ReactNode } from 'react'
+import { VariantProps } from 'class-variance-authority'
+
+import { Button, buttonVariants } from '@/components/ui/button'
+
+interface Props {
+  onClick?: () => void
+  children?: ReactNode
+  TextAction: string
+  disabled?: boolean
+  variant?: VariantProps<typeof buttonVariants>['variant']
+  size?: VariantProps<typeof buttonVariants>['size']
+}
+
+export const ActionButton: React.FC<Props> = ({
+  onClick,
+  TextAction,
+  disabled,
+  variant,
+  size,
+  children,
+}) => {
+  return (
+    <Button onClick={onClick} disabled={disabled} variant={variant} size={size}>
+      {TextAction} {children}
+    </Button>
+  )
+}
---
+++ src/components/navbar/NavBar.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+
+// import { ThemeToggle } from '@/components/navbar/theme-toggle';
+
+export const Navbar = async () => {
+  return (
+    <header className="w-full border-b">
+      <div className="container flex h-16 items-center justify-between">
+        <Link href="/" className="font-mono text-sm font-thin">
+          <p>
+            By <span>Mr.Archer</span>
+          </p>
+        </Link>
+        <div className="flex items-center gap-2">{/* <ThemeToggle /> */}</div>
+      </div>
+    </header>
+  )
+}
