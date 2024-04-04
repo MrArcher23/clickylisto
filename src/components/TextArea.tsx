553b98170eba7d39b36ff2b882f8b5483a922ff7
Raul Gallegos <rgh2309@gmail.com>
2024-04-03 18:57:23 -0600

    ajustes a pagina de linktows

--- src/components/TextArea.tsx
+++ src/components/TextArea.tsx
@@ -3,9 +3,18 @@ import { Textarea } from "@/components/ui/textarea";
 interface Props {
   texto: string;
   placeholder: string;
+  className?: string;
   handleChange?: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
 }
 
-export const TextArea: React.FC<Props> = ({ texto, placeholder, handleChange }) => {
-  return <Textarea value={texto} placeholder={placeholder} onChange={handleChange} rows={6} />;
+export const TextArea: React.FC<Props> = ({ texto, placeholder, handleChange, className }) => {
+  return (
+    <Textarea
+      className={className}
+      value={texto}
+      placeholder={placeholder}
+      onChange={handleChange}
+      rows={6}
+    />
+  );
 };
