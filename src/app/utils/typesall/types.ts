8ad9b2cfd0a31942a1d5f978456a33e58b324f56
Raul Gallegos <rgh2309@gmail.com>
2024-04-06 18:43:28 -0600

    probando carpeta de utils data types

---
+++ src/app/utils/typesall/types.ts
@@ -0,0 +1,13 @@
+// types/types.ts
+
+export interface Step {
+  id: number;
+  text: string;
+}
+
+export interface countryCodes {
+  id: number;
+  value: string;
+  label: string;
+  countryCode: string;
+}
