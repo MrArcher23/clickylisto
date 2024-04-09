24931587953951e93516baf69f46052308bccb59
Raul Gallegos <rgh2309@gmail.com>
2024-04-08 20:21:20 -0600

    fix uuid

--- src/app/uuidgen/UuidGenClientComponent.tsx
+++ src/app/uuidgen/UuidGenClientComponent.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { ActionButton } from "@/components/ActionButton";
 import { Input } from "@/components/ui/input";
